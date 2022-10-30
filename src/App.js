73476a2a17bc802c872399564c578b3e3c7c904f
mima ch <mima.cherfa19@hotmail.fr>
2022-10-30 17:37:03 +0100

    page erreur

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" Exact element={<Home />} />
-                <Route path="/about" Exact element={<About />} />
+                <Route path="/" exact element={<Home />} />
+                <Route path="/about" exact element={<About />} />
                 {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
                 <Route path="*" element={<Error />} />
             </Routes>
