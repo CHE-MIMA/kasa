fb2b19e333b095fabdcbf5ef4bc5ff648e1d5473
mima ch <mima.cherfa19@hotmail.fr>
2022-11-19 13:38:58 +0100

    fin erreur logof

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import kasaF from '../assets/logof.png'
+import kasaF from '../assets/logoFooter.png'
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='footer-container'>
-                <img src={kasaF} alt="" />
+                <img src={kasaF} alt="kasa" />
             </div>
             <p> &copy; 2020 Kasa. All rights reserved</p>
 
